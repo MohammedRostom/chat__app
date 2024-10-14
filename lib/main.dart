ed8e23a1d6c2261a98127f9d6f70580004bbbcf8
MohammedRostom <mohammedrostom011@gmail.com>
2024-10-14 12:51:23 +0300

    add some thinks

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,7 @@ import 'package:apply_pagenation/core/Theme/Style.dart';
 import 'package:apply_pagenation/core/routing/routing.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
-import 'features/Auth/peresentaion/view/splach/Splach_Screen.dart';
+import '../../generated/l10n.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,15 +19,14 @@ class MyApp extends StatelessWidget {
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) => MaterialApp(
-        locale: Locale("en"),
-        localizationsDelegates: [
-          S.delegate,
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-        ],
-        supportedLocales: S.delegate.supportedLocales,
-        debugShowCheckedModeBanner: false,
+        // locale: Locale("en"),
+        // localizationsDelegates: [
+        //   S.delegate,
+        //   GlobalMaterialLocalizations.delegate,
+        //   GlobalWidgetsLocalizations.delegate,
+        //   GlobalCupertinoLocalizations.delegate,
+        // ],
+        // supportedLocales: S.delegate.supportedLocales,
         debugShowCheckedModeBanner: false,
         routes: Routeing.MapOfAppRoutes,
         initialRoute: Routeing.Start,
