79ff6e75dc4f23695d3f71112c0ce71ed9a236d4
MohammedRostom <mohammedrostom011@gmail.com>
2024-10-14 13:08:09 +0300

    refactor Code

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:apply_pagenation/core/routing/routing.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../generated/l10n.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,22 +19,33 @@ class MyApp extends StatelessWidget {
       designSize: Size(360, 690),
       minTextAdapt: true,
       splitScreenMode: true,
-      builder: (context, child) => MaterialApp(
-        // locale: Locale("en"),
-        // localizationsDelegates: [
-        //   S.delegate,
-        //   GlobalMaterialLocalizations.delegate,
-        //   GlobalWidgetsLocalizations.delegate,
-        //   GlobalCupertinoLocalizations.delegate,
-        // ],
-        // supportedLocales: S.delegate.supportedLocales,
-        debugShowCheckedModeBanner: false,
-        routes: Routeing.MapOfAppRoutes,
-        initialRoute: Routeing.Start,
-        theme: AppTheme.LighTheme,
-        darkTheme: AppTheme.DarkTheme,
-        themeMode: ThemeMode.light,
-      ),
+      builder: (context, child) => matreial_app(),
+    );
+  }
+}
+
+class matreial_app extends StatelessWidget {
+  const matreial_app({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      locale: Locale("en"),
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      debugShowCheckedModeBanner: false,
+      routes: Routeing.MapOfAppRoutes,
+      initialRoute: Routeing.Start,
+      theme: AppTheme.LighTheme,
+      darkTheme: AppTheme.DarkTheme,
+      themeMode: ThemeMode.light,
     );
   }
 }
