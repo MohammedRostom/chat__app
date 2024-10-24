0d17abc27e0c986ff79e54249370e9e6756d0fb5
MohammedRostom <mohammedrostom011@gmail.com>
2024-10-24 23:05:25 +0300

    merging to dev

--- lib/features/Auth/peresentaion/viewModel/cubit/onbourd_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-
-part 'onbourd_state.dart';
-
-class OnbourdCubit extends Cubit<OnbourdState> {
-  OnbourdCubit() : super(OnbourdInitial());
-}
